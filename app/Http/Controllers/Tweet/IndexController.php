2eae1edb866df5d8ea1c09212cf9a38bb35c22e0
mae616 <50073576+mae616@users.noreply.github.com>
2022-09-19 17:07:53 +0900

    02-02 つぶやき投稿

---
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Tweet;
+
+use App\Models\Tweet;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $tweets = Tweet::orderBy('created_at', 'desc')->get();
+
+        return view('tweet.index')
+            ->with('tweets', $tweets);
+    }
+}
---
+++ database/seeders/TweetsSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Tweet;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class TweetsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Tweet::factory()->count(10)->create();
+    }
+}
